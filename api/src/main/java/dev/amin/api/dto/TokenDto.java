a8bab3c40776cb51bccc5135d1772555394ce93b
aminnorouzi <aminnorouzidev@gmail.com>
2025-10-05 23:30:34 +0330

    add services dtos and logs

--- api/src/main/java/dev/amin/api/dto/TokenDto.java
+++ api/src/main/java/dev/amin/api/dto/TokenDto.java
@@ -2,10 +2,12 @@ package dev.amin.api.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
 @AllArgsConstructor
-public class TokenResponse {
+public class TokenDto {
 
     private String token;
 }
